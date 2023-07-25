8b36d023fc117254eb1c51b4632619ffe3be4d52
fahrysrg <fahryrozysrg@gmail.com>
2023-07-25 20:36:07 +0700

    setup challenge 8 (temporary)

---
+++ react/src/App.jsx
@@ -0,0 +1,15 @@
+import EditPlayer from "./components/EditPlayer";
+import SearchPlayer from "./components/SearchPlayer";
+import Register from "./components/register";
+
+function App() {
+  return (
+    <>
+      <Register />
+      <EditPlayer />
+      <SearchPlayer />
+    </>
+  );
+}
+
+export default App;
---
+++ react/src/components/SearchPlayer.jsx
@@ -0,0 +1,113 @@
+export default function SearchPlayer() {
+  return (
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            className="mx-auto h-100 w-auto"
+            src="https://global-uploads.webflow.com/6100d0111a4ed76bc1b9fd54/62149cbcd4342e531f3c4241_Binar%20academy.png"
+            alt="Your Company"
+          />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Input your criteria
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" action="#" method="POST">
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Email Address
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="px-5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="username"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Username
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="username"
+                  name="username"
+                  type="text"
+                  autoComplete="username"
+                  required
+                  className="px-5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="exp"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Experience
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="exp"
+                  name="exp"
+                  type="text"
+                  required
+                  className="px-5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="lvl"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Level
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="lvl"
+                  name="lvl"
+                  type="text"
+                  required
+                  className="px-5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Search
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
